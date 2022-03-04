a691ade332699b1669b75fbd0f39b72f6512a653
faithSchwinger4 <faith.cox104@gmail.com>
2022-03-04 00:26:04 -0500

    First Save

---
+++ src/controller/LoginScreenController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginScreenController implements Initializable {
+
+    public TextField username;
+    public TextField password;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void onActionLoginButtonPressed(ActionEvent actionEvent) throws IOException {
+        // take in username, password
+        // check db for validity
+        // if valid, next screen
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 400, 500);
+        stage.setTitle("Main Menu");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
