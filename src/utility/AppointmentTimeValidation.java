b354025acecb8beb81aabde2c3f3884e7f0568ed
faithSchwinger4 <faith.cox104@gmail.com>
2022-03-25 16:35:32 -0400

    update

--- src/utility/AppointmentTimeValidation.java
+++ src/utility/AppointmentTimeValidation.java
@@ -1,7 +1,39 @@
 package utility;
 
+import javafx.collections.ObservableList;
+import model.Appointment;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
 public class AppointmentTimeValidation {
 
+    public static boolean noConflictingAppointment(int customerId, LocalDateTime start, LocalDateTime end) throws SQLException {
+        // get all the appointments for once customer
+        ObservableList<Appointment> customerAppointments = AppointmentQuery.getCustomerAppointments(customerId);
 
+        for (Appointment appointment : customerAppointments) {
+            if (start.isAfter(appointment.getStart()) && start.isBefore(appointment.getEnd())) {
+                return false;
+            }
+            if (start.isEqual(appointment.getStart()) || end.isEqual(appointment.getEnd())) {
+                return false;
+            }
+            if (end.isAfter(appointment.getStart()) && end.isBefore(appointment.getEnd())) {
+                return false;
+            }
+        }
+
+        //there was no conflict, so it can continue
+        return true;
+    }
+
+    public static boolean duringBusinessHours(LocalDateTime start, LocalDateTime end) {
+        // check the time zone against EST
+        // get the computers current time zone?
+        //if () {}
+
+        return true;
+    }
 
 }
