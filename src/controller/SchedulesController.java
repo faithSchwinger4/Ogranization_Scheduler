74ae914b3d5c34ddb6518f099276e3b38fe68492
faithSchwinger4 <faith.cox104@gmail.com>
2022-03-25 02:21:15 -0400

    update

--- src/controller/SchedulesController.java
+++ src/controller/SchedulesController.java
@@ -1,15 +1,80 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Appointment;
+import model.Contact;
+import utility.AppointmentQuery;
+import utility.ContactQuery;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ResourceBundle;
 
-public class SchedulesController {
+public class SchedulesController implements Initializable {
+
+    public ComboBox<Contact> contactComboBox;
+
+    @FXML
+    private TableView<Appointment> appointmentTable;
+    @FXML
+    private TableColumn<Appointment, Integer> appointmentIdColumn;
+    @FXML
+    private TableColumn<Appointment, String> titleColumn;
+    @FXML
+    private TableColumn<Appointment, String> descriptionColumn;
+    @FXML
+    private TableColumn<Appointment, String> typeColumn;
+    @FXML
+    private TableColumn<Appointment, Timestamp> startColumn;
+    @FXML
+    private TableColumn<Appointment, Timestamp> endColumn;
+    @FXML
+    private TableColumn<Appointment, Integer> customerIdColumn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<Contact> allContacts = null;
+        try {
+            allContacts = ContactQuery.getAllContacts();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        for (Contact contact : allContacts) {
+            contactComboBox.getItems().add(contact);
+        }
+
+        //fill each column
+        appointmentIdColumn.setCellValueFactory(new PropertyValueFactory<>("appointmentId"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        startColumn.setCellValueFactory(new PropertyValueFactory<>("start"));
+        endColumn.setCellValueFactory(new PropertyValueFactory<>("end"));
+        customerIdColumn.setCellValueFactory(new PropertyValueFactory<>("customerId"));
+    }
+
+    public void onActionContactChosen(ActionEvent actionEvent) throws SQLException {
+        int selectedContactId = contactComboBox.getValue().getContactId();
+        ObservableList<Appointment> contactAppointments = AppointmentQuery.getContactAppointments(selectedContactId);
+
+        appointmentTable.setItems(contactAppointments);
+    }
+
     public void onActionReturnToMainMenuButtonPressed(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
         Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
--- src/utility/AppointmentQuery.java
+++ src/utility/AppointmentQuery.java
@@ -103,6 +103,39 @@ public class AppointmentQuery {
         return customerAppointments;
     }
 
+    public static ObservableList<Appointment> getContactAppointments(int contactId) throws SQLException {
+        ObservableList<Appointment> contactAppointments = FXCollections.observableArrayList();
+
+        String sql = "SELECT Appointment_ID, Title, Description, Location, Type, Start, End, Create_Date, Created_By, " +
+                "Last_Update, Last_Updated_By, Customer_ID, User_ID, Appointments.Contact_ID, Contact_Name " +
+                "FROM Appointments INNER JOIN Contacts ON Appointments.Contact_ID = Contacts.Contact_ID " +
+                "WHERE Appointments.Contact_ID = ?";
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        ps.setInt(1, contactId);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()) {
+            int appointmentId = rs.getInt("Appointment_ID");
+            String title = rs.getString("Title");
+            String description = rs.getString("Description");
+            String location = rs.getString("Location");
+            String type = rs.getString("Type");
+            LocalDateTime start = rs.getTimestamp("Start").toLocalDateTime();
+            LocalDateTime end = rs.getTimestamp("End").toLocalDateTime();
+            LocalDateTime createDate = rs.getTimestamp("Create_Date").toLocalDateTime();
+            String createdBy = rs.getString("Created_By");
+            LocalDateTime lastUpdate = rs.getTimestamp("Last_Update").toLocalDateTime();
+            String lastUpdatedBy = rs.getString("Last_Updated_By");
+            int userId = rs.getInt("User_ID");
+            int customerId = rs.getInt("Customer_ID");
+            String contactName = rs.getString("Contact_Name");
+            Appointment newAppointment = new Appointment(appointmentId, title, description, location, type, start, end,
+                    createDate, createdBy, lastUpdate, lastUpdatedBy, customerId, userId, contactId, contactName);
+            contactAppointments.add(newAppointment);
+        }
+
+        return contactAppointments;
+    }
+
     public static int update(int appointmentId, String title, String description, String location, String type, Timestamp start,
                              Timestamp end, Timestamp lastUpdate, String lastUpdatedBy, int customerId, int userId,
                              int contactId) throws SQLException {
