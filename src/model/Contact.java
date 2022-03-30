351dc56164a583c295955c037c9c14821ec9fad9
faithSchwinger4 <faith.cox104@gmail.com>
2022-03-29 20:47:22 -0700

    more JavaDocs completed and updated

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -1,40 +1,39 @@
 package model;
 
+/** This class creates Contact objects. Each Contact object represents one contact existing in the database. */
 public class Contact {
+
+    /** Stores a contact's ID number. */
     private int contactId;
+
+    /** Stores a contact's name. */
     private String contactName;
+
+    /** Stores a contact's email. */
     private String email;
 
+
+    /** Initializes a contact with an ID, name, and email. */
     public Contact(int contactId, String contactName, String email) {
         this.contactId = contactId;
         this.contactName = contactName;
         this.email = email;
     }
 
+    /** This function gets a contact's ID number.
+     * @return a contact's ID number */
     public int getContactId() {
         return contactId;
     }
 
+    /** This function sets a contact's ID number.
+     * @param contactId sets a contact's ID number */
     public void setContactId(int contactId) {
         this.contactId = contactId;
     }
 
-    public String getContactName() {
-        return contactName;
-    }
-
-    public void setContactName(String contactName) {
-        this.contactName = contactName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+    /** This function overrides the toString method so that it returns the contactName.
+     * @return the name of the contact */
     @Override
     public String toString() {
         return contactName;
--- src/model/Country.java
+++ src/model/Country.java
@@ -3,14 +3,29 @@ package model;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+/** This class creates Country objects. Each Country object represents one country existing in the database. */
 public class Country {
+
+    /** Stores a country's ID number. */
     private int countryId;
+
+    /** Stores a country's name. */
     private String countryName;
+
+    /** Stores the date and time a country was created in the database. */
     private Timestamp createDate;
+
+    /** Stores who created a country in the database. */
     private String createdBy;
+
+    /** Stores the date and time a country was last updated in the database. */
     private LocalDateTime lastUpdate;
+
+    /** Stores who last updated a country in the database. */
     private String lastUpdatedBy;
 
+
+    /** Initializes a country object with a parameter for each member of the Country class. */
     public Country(int countryId, String countryName, Timestamp createDate, String createdBy, LocalDateTime lastUpdate, String lastUpdatedBy) {
         this.countryId = countryId;
         this.countryName = countryName;
@@ -20,54 +35,27 @@ public class Country {
         this.lastUpdatedBy = lastUpdatedBy;
     }
 
+    /** This function gets a country's ID number.
+     * @return a country's ID number */
     public int getCountryId() {
         return countryId;
     }
 
-    public void setCountryId(int countryId) {
-        this.countryId = countryId;
-    }
-
-    public String getCountryName() {
-        return countryName;
-    }
-
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
-    }
-
-    public Timestamp getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Timestamp createDate) {
-        this.createDate = createDate;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
+    /** This method gets the last date and time the country was updated in the database.
+     * @return the last date and time the country was updated */
     public LocalDateTime getLastUpdate() {
         return lastUpdate;
     }
 
+    /** This method sets the last date and time the country was updated in the database.
+     * @param lastUpdate sets the last date and time the country was updated */
     public void setLastUpdate(LocalDateTime lastUpdate) {
         this.lastUpdate = lastUpdate;
     }
 
-    public String getLastUpdatedBy() {
-        return lastUpdatedBy;
-    }
 
-    public void setLastUpdatedBy(String lastUpdatedBy) {
-        this.lastUpdatedBy = lastUpdatedBy;
-    }
-
+    /** This function overrides the toString method so that it returns the country's name.
+     * @return the name of the country */
     @Override
     public String toString() {
         return countryName;
