4c9ff624467d5fe02dc8aecc5191b243bea404e6
faithSchwinger4 <faith.cox104@gmail.com>
2022-03-29 21:20:30 -0700

    more JavaDocs completed and updated

--- src/model/User.java
+++ src/model/User.java
@@ -1,9 +1,18 @@
 package model;
 
+/** This class creates User objects. Each User object represents one user existing in the database. */
 public class User {
+
+
     private int userId;
+
+
     private String userName;
+
+
     private String password;
+
+
 
     public User(int userId, String userName, String password) {
         this.userId = userId;
@@ -11,21 +20,26 @@ public class User {
         this.password = password;
     }
 
+
     public int getUserId() {
         return userId;
     }
+
 
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
+
     public String getUserName() {
         return userName;
     }
 
+
     public void setUserName(String userName) {
         this.userName = userName;
     }
+
 
     @Override
     public String toString() {
